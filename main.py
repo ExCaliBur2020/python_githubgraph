35e3722e76f7d880117ef5b7600a569b01ce4574
Yash Raut <yash@koyaltechsolutions.com>
2021-10-09 19:10:22 +0530

    added config

--- main.py
+++ main.py
@@ -1,16 +1,9 @@
-#importing operating system package.
 import os
 
-## Number of days you want to make commits
-for i in range(1,365 + 1):
-    d = str(i) + ' day ago'
-    ## Open a text file and modify it
-    with open('load.txt', 'a') as file:
+for i in range(1, 365 + 1):
+    d = str(i) + " day ago"
+    with open("load.txt", "a") as file:
         file.write(d)
-    ## Add load.txt to staging area
-    os.system('git add load.txt')
-    ## Commit it
+    os.system("git add load.txt")
     os.system('git commit --date="' + d + '" -m "first commit"')
-
-## push the commit to github
-os.system('git push -u origin master')  
+os.system("git push -u origin master")
